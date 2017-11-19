9cc9de30a0196e632d11c3fb1f5c4d673c595f9a
DarkKnazz <zhenyaknazz@gmail.com>
2017-11-19 23:18:07 +0300

    Adding checkbox to todos

--- SourceCode/app/components/TodoApp.jsx
+++ SourceCode/app/components/TodoApp.jsx
@@ -12,16 +12,20 @@ var TodoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: "Walk the dog"
+          text: "Walk the dog",
+          completed: false
         }, {
           id: uuid(),
-          text: "Clean the street"
+          text: "Clean the street",
+          completed: true
         }, {
           id: uuid(),
-          text: "Do homework"
+          text: "Do homework",
+          completed: true
         }, {
           id: uuid(),
-          text: "Sleep 8 hours"
+          text: "Sleep 8 hours",
+          completed: false
         }
       ]
     }
@@ -32,10 +36,22 @@ var TodoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
+  },
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+
+      return todo;
+    })
+
+    this.setState({ todos: updatedTodos })
   },
   handleSearch: function (showCompleted, searchText) {
     this.setState({
@@ -48,7 +64,7 @@ var TodoApp = React.createClass({
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch} />
-        <TodoList todos={todos} />
+        <TodoList todos={todos} onToggle={this.handleToggle} />
         <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     )
